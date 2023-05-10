fa6a213029292ce834c7bafc7d26fcdb6a57c480
Khine <khinezar70122107@gmail.com>
2023-05-09 15:07:05 -1200

    first commit

---
+++ Basic/1.cs
@@ -0,0 +1,11 @@
+//to output
+using System;
+using Internal;
+
+public class standardOutput
+{
+    public static void Main(string[] args){
+        Console.WriteLine("Hello");
+        Console.WriteLine("Hello");
+    }
+}
