da1bd23bfcfaed19230c2da1190fa6d5530b51d0
Khine <khinezar70122107@gmail.com>
2023-06-08 18:26:38 -1200

    for loop

--- FlowControl/1.ifStatement.cs
+++ FlowControl/1.ifStatement.cs
@@ -1,5 +1,4 @@
 using System;
-using Internal;
 
 public class ifStatement
 {
---
+++ FlowControl/7.forLoop.cs
@@ -0,0 +1,15 @@
+using System;
+using Internal;
+//for(initialization;condition;iterator)
+//{
+//   body of for loop
+//}
+
+public class forLoop
+{
+    public static void Main(string[] args){
+          for(int i=1;i<=5;++i){
+            Console.WriteLine("C# For Loop: Iteration "+i);
+          }
+    }
+}
