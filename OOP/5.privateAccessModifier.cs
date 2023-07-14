c35a5e2b1dc50880c47268d174a1b1a55d40e9da
Khine <khinezar70122107@gmail.com>
2023-07-13 21:24:48 -1200

    private access modifier

---
+++ OOP/5.privateAccessModifier.cs
@@ -0,0 +1,21 @@
+using System;
+
+//private access modifier --> it can only be accessed within the same class
+public class privateAccessModifier
+{
+    class Student{
+        private string name = "C#";
+
+        public void print(){
+            Console.WriteLine("Hello From Student Class" +name);
+        }
+    }
+    class Program{
+        public static void Main(string[] args){
+            Student stu1 = new Student();
+            // Console.WriteLine("Name:" + stu1.name);
+
+            stu1.print();
+        }
+    }
+}
